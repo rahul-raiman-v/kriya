8841520af3560cfe688f6fd5c318a821c2bd79bc
deepakgs536 <deepakgs536@gmail.com>
2025-08-09 19:17:26 +0530

    demo teams page completed

---
+++ frontend/src/assets/organizationIcon.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export const OrganizationIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="#e3e3e3"
+    viewBox="0 -960 960 960"
+    {...props}
+  >
+    <path d="M80-120v-720h400v160h400v560zm80-80h80v-80h-80zm0-160h80v-80h-80zm0-160h80v-80h-80zm0-160h80v-80h-80zm160 480h80v-80h-80zm0-160h80v-80h-80zm0-160h80v-80h-80zm0-160h80v-80h-80zm160 480h320v-400H480v80h80v80h-80v80h80v80h-80zm160-240v-80h80v80zm0 160v-80h80v80z"></path>
+  </svg>
+);
--- frontend/src/routes/appRouter/index.jsx
+++ frontend/src/routes/appRouter/index.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
 import { routes, PrivateRouter, PublicRouter } from '..';
-import { HomePage } from '../../pages';
+import { HomePage, TeamsPage } from '../../pages';
 
 export const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path={routes.home} element={<HomePage />} />
+        {/* <Route path={routes.home} element={<HomePage />} /> */}
+        <Route path={routes.home} element={<TeamsPage />} />
       </Routes>
     </Router>
   );
