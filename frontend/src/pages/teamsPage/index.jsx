8672e0a5bb02b32293fbedb58c6651cb7c66ad64
Rahul P <rahulraiman3#gmail.com>
2025-08-20 09:33:01 +0530

    website second deployment

--- frontend/src/pages/teamsPage/index.jsx
+++ frontend/src/pages/teamsPage/index.jsx
@@ -101,6 +101,15 @@ export const TeamsPage = () => {
       phone: '+91 8344480485',
       email: 'prathap@bitsathy.ac.in',
     },
+    {
+      id: 10,
+      name: 'Dr Murugappan S',
+      designation: '',
+      image:
+        'https://www.bitsathy.ac.in/wp-content/uploads/Dr-Murugappan-S.jpg',
+      phone: '+91 8344480485',
+      email: 'murugappans@bitsathy.ac.in',
+    },
   ];
 
   // const faculties = [
